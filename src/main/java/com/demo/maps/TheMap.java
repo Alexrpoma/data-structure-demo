8d9bd801e02ebd969747778faccc86f425fd8aab
Alex Poma <alex.rpoma@outlook.com>
2023-05-18 16:50:31 -0400

    HashCode and Equals in Map

--- src/main/java/com/demo/maps/TheMap.java
+++ src/main/java/com/demo/maps/TheMap.java
@@ -2,6 +2,7 @@ package com.demo.maps;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 public class TheMap {
 
@@ -21,8 +22,55 @@ public class TheMap {
 
     System.out.println(map.getOrDefault(6, new Person("Default")));
 
+    //Map with objects:
+    Rol admin = new Rol("admin");
+    Rol sysAdmin = new Rol("sysAdmin");
+    Rol manager = new Rol("manager");
+    Rol admin2 = new Rol("admin"); //Not allowed two admin -> its necessary override hashCode and equals methods.
+
+    System.out.println(admin.equals(admin2)); // --> true
+
+    Map<Rol, Person> employees = new HashMap<>();
+    employees.put(admin, new Person("daniel"));
+    employees.put(sysAdmin, new Person("marian"));
+    employees.put(manager, new Person("oscar"));
+    employees.put(admin2, new Person("carl"));
+
+    System.out.println();
+    System.out.println(employees);
+
   }
 
   record Person(String name) {}
+
+  static class Rol {
+    String name;
+
+    public Rol(String name) {
+      this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+      if (this == o) return true;
+      if (o == null || getClass() != o.getClass()) return false;
+
+      Rol rol = (Rol) o;
+
+      return Objects.equals(name, rol.name);
+    }
+
+    @Override
+    public int hashCode() {
+      return name != null ? name.hashCode() : 0;
+    }
+
+    @Override
+    public String toString() {
+      return "Rol{" +
+        "name='" + name + '\'' +
+        '}';
+    }
+  }
 
 }
